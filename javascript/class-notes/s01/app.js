9ed697cd12dbaa02772909c127f401ece21d6a77
Matthew D <67597416+cwmatthew@users.noreply.github.com>
2022-08-06 12:41:16 +0300

    merhaba js

---
+++ javascript/class-notes/s01/app.js
@@ -0,0 +1,7 @@
+console.log('app.js is loaded');
+
+// let firstName = prompt('Enter your name:');
+// alert('firstName :' + firstName);
+
+let sonuc = confirm('are you legal?');
+console.log(sonuc);
