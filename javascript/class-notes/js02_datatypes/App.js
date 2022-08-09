5cdb3a3b78ae254086925ad5e06419e1e029851d
Mark-Mad <mark.m@clarusway.com>
2022-08-09 21:15:58 +0300

    2. ders başı

--- javascript/class-notes/js02_datatypes/App.js
+++ javascript/class-notes/js02_datatypes/App.js
@@ -1,1 +1,36 @@
-console.log('App js Running')
+// console.log('App js Running')
+
+
+// var a = 5;
+// console.log(a);
+
+// {
+//     a = 3;
+//     console.log(a);
+// }
+
+// var a = 5;
+// console.log(a);
+
+/* console.log(typeof 0)
+console.log(typeof 3.14)
+console.log(typeof 'Hello')
+console.log(typeof (8 > 10))
+console.log(8 > 10)
+console.log(typeof '') */
+
+// let b = +prompt('Bir rakam giriniz')
+
+// console.log(b, typeof b)
+
+// console.log( typeof 'number')
+
+let age;
+
+console.log(age)
+
+console.log(myAge)
+
+let myAge = 40;
+
+console.log('Hello')
