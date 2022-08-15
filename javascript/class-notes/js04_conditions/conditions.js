2108549e524d90a333aefd39cc9446c3726b1154
profnoah <noah@clarusway.com>
2022-08-15 22:34:20 +0300

    if-else

--- javascript/class-notes/js04_conditions/conditions.js
+++ javascript/class-notes/js04_conditions/conditions.js
@@ -21,14 +21,28 @@ console.log("****** CONDITIONS *******");
 
 //? ORNEK2: console'dan 3 tamsayi alarak bunlarin en buyugunu
 //? yazdiriniz.
-const n1 = prompt("Sayi1:");
-const n2 = prompt("Sayi2:");
-const n3 = prompt("Sayi3:");
+const n1 = +prompt("Sayi1:");
+const n2 = +prompt("Sayi2:");
+const n3 = +prompt("Sayi3:");
 
-if (n1 >= n2 && n1 >= n3) {
-  console.log(`${n1} en buyuk sayidir`);
-} else if (n2 >= n1 && n2 >= n3) {
-  console.log(`${n2} en buyuk sayidir`);
-} else if (n3 >= n1 && n3 >= n2) {
-  console.log(`${n3} en buyuk sayidir`);
+//* 1.Yontem
+// if (n1 >= n2 && n1 >= n3) {
+//   console.log(`${n1} en buyuk sayidir`);
+// } else if (n2 >= n1 && n2 >= n3) {
+//   console.log(`${n2} en buyuk sayidir`);
+// } else if (n3 >= n1 && n3 >= n2) {
+//   console.log(`${n3} en buyuk sayidir`);
+// }
+
+//* 2.Yontem
+let enBuyuk = n1;
+
+if (n2 >= enBuyuk) {
+  enBuyuk = n2;
 }
+if (n3 >= enBuyuk) {
+  enBuyuk = n3;
+}
+console.log(`${enBuyuk} en buyuktur`);
+
+//? ODEV1: Dort Islem Hesap Makinasi
