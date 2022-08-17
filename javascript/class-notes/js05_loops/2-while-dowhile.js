5510dab4ca2fb057114bc08f0013210d3203538a
profnoah <noah@clarusway.com>
2022-08-17 22:01:52 +0300

    while loops

--- javascript/class-notes/js05_loops/2-while-dowhile.js
+++ javascript/class-notes/js05_loops/2-while-dowhile.js
@@ -7,3 +7,22 @@ console.log("****** WHILE *******");
 //? ORNEK: Kullanicidan 0-100 arasinda bir not isteyen ve girilen not
 //? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
 //? not girmeye zorlayan kodu while dongusu ile yaziniz.
+
+let not = +prompt("Lutfen 0-100 arasinda bir not girniz.");
+
+while (not < 0 || not > 100) {
+  console.log("Not 0-100 arasinda olmalidir");
+  not = prompt("Lutfen 0-100 arasinda bir not giriniz.");
+}
+
+console.log("Giridiginiz Not:", not);
+
+// if (not < 0 || not > 100) {
+//   alert("Not 0-100 arasinda olmalidir");
+// }
+
+// * ============================================
+// *         DONGULER(LOOPS)- DO-WHILE
+// * ============================================
+
+console.log("****** DO-WHILE *******");
