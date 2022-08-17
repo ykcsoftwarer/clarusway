6d124d4933d0d370ded27f3dce29c38543879bab
profnoah <noah@clarusway.com>
2022-08-17 22:44:24 +0300

    guest game started

---
+++ javascript/class-notes/js05_loops/3-guestGame.js
@@ -0,0 +1,1 @@
+//* Program 0*100 arasında rasgele bir sayı tutacak ve kullanıcının bu sayıyı 5 kere(hak) de bilmesini isteyecektir. Her yanlışta hakkını bir düşürecek ve ARTTIR/AZALT diyerek kullanıcıyı yönlendirecektir.Sonuç olarak kullanıcının hakkı 0 olursa "Üzgünüz bilemediniz" eğer bildi ise "Tebrikler ... denemede bildiniz" yazacaktır.
